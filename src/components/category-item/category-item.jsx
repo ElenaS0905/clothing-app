158232ece1d6228beaa98590bb7b43e9e324b4a5
Elena Spînu <spinuelena03@gmail.com>
2023-03-28 17:17:10 +0300

    add(routs to ShopPage)

--- src/components/category-item/category-item.jsx
+++ src/components/category-item/category-item.jsx
@@ -1,16 +1,21 @@
-import './category-item.style.scss';
+import "./category-item.style.scss";
 
-const CategoryItem = ({category}) =>{
-    const {imageUrl, title} = category;
-            return  <div className="category-container" >
-                <div className='background-image'  style={{
-                    backgroundImage: `url(${imageUrl})`}
-                }/>
-                <div className="category-body-container">
-                    <h2>{title}</h2>
-                    <p>Shop now</p>
-                </div>
-            </div>
-}
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+};
 
 export default CategoryItem;
