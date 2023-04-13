190ed3a7dbb6087460e0323edb47ecd6729b71a6
Elena Spînu <spinuelena03@gmail.com>
2023-04-13 12:03:40 +0300

    finish(style)

--- src/context/cart-context.jsx
+++ src/context/cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useReducer } from "react";
 
 const addCartItem = (cartItem, productToAdd) => {
   const existingCartItem = cartItem.find(
@@ -44,11 +44,46 @@ export const CartContext = createContext({
   total: 0,
 });
 
+export const CART_ACTIONS_TYPES = {
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_CART_COUNT: "SET_CART_ITEMS",
+  SET_TOTAL: "SET_TOTAL",
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  total: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+  // const payload = {
+  //   cartItems,
+  //   cartCount,
+  //   total,
+  // };
+
+  switch (type) {
+    case "SET_CART_ITEMS":
+      return {
+        ...state,
+        ...payload,
+      };
+    default: {
+      throw new Error(`error occurred in cartReducer unhandled ${type} `);
+    }
+  }
+};
+
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
   const [total, seTotal] = useState(0);
+  const [dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
   useEffect(() => {
     const newTotal = cartItems.reduce(
@@ -65,6 +100,8 @@ export const CartProvider = ({ children }) => {
     );
     setCartCount(newCartCount);
   }, [cartItems]);
+
+  const updateCartItemsReducer = (newCartItems) => {};
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
@@ -88,6 +125,99 @@ export const CartProvider = ({ children }) => {
     cartCount,
     total,
   };
-
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+
+// import { createContext, useEffect, useState } from "react";
+//
+// const addCartItem = (cartItem, productToAdd) => {
+//   const existingCartItem = cartItem.find(
+//       (cartItem) => cartItem.id === productToAdd.id
+//   );
+//
+//   if (existingCartItem) {
+//     return cartItem.map((cartItem) =>
+//         cartItem.id === productToAdd.id
+//             ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//             : cartItem
+//     );
+//   }
+//   return [...cartItem, { ...productToAdd, quantity: 1 }];
+// };
+//
+// export const removeCartItem = (cartItems, productToRemove) => {
+//   const existingCartItem = cartItems.find(
+//       (cartItem) => cartItem.id === productToRemove.id
+//   );
+//
+//   if (existingCartItem.quantity === 1) {
+//     return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+//   }
+//   return cartItems.map((cartItem) =>
+//       cartItem.id === productToRemove.id
+//           ? { ...cartItem, quantity: cartItem.quantity - 1 }
+//           : cartItem
+//   );
+// };
+//
+// export const clearCartItem = (cartItems, cartToClear) =>
+//     cartItems.filter((cartItem) => cartItem.id !== cartToClear.id);
+//
+// export const CartContext = createContext({
+//   isCartOpen: false,
+//   setIsCartOpen: () => {},
+//   cartItems: [],
+//   addItemToCart: () => {},
+//   removeItemCart: () => {},
+//   clearCart: () => {},
+//   cartCount: 0,
+//   total: 0,
+// });
+//
+// export const CartProvider = ({ children }) => {
+//   const [isCartOpen, setIsCartOpen] = useState(false);
+//   const [cartItems, setCartItems] = useState([]);
+//   const [cartCount, setCartCount] = useState(0);
+//   const [total, seTotal] = useState(0);
+//
+//   useEffect(() => {
+//     const newTotal = cartItems.reduce(
+//         (total, cartItem) => total + cartItem.quantity * cartItem.price,
+//         0
+//     );
+//     seTotal(newTotal);
+//   }, [cartItems]);
+//
+//   useEffect(() => {
+//     const newCartCount = cartItems.reduce(
+//         (total, cartItem) => total + cartItem.quantity,
+//         0
+//     );
+//     setCartCount(newCartCount);
+//   }, [cartItems]);
+//
+//   const addItemToCart = (productToAdd) => {
+//     setCartItems(addCartItem(cartItems, productToAdd));
+//   };
+//
+//   const removeItemCart = (productToRemove) => {
+//     setCartItems(removeCartItem(cartItems, productToRemove));
+//   };
+//
+//   const clearCart = (cartToClear) => {
+//     setCartItems(clearCartItem(cartItems, cartToClear));
+//   };
+//
+//   const value = {
+//     isCartOpen,
+//     setIsCartOpen,
+//     addItemToCart,
+//     removeItemCart,
+//     clearCart,
+//     cartItems,
+//     cartCount,
+//     total,
+//   };
+//
+//   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+// };
--- src/context/categories-context.jsx
+++ src/context/categories-context.jsx
@@ -1,12 +1,46 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase-utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+// export const CATEGORY_ACTIONS_TYPES = {
+//   SET_CATEGORIES_MAP: "SET_CATEGORIES_MAP",
+// };
+//
+// const categoryReducer = (state, action) => {
+//   const { type, payload } = action;
+//
+//   switch (type) {
+//     case CATEGORY_ACTIONS_TYPES.SET_CATEGORIES_MAP: {
+//       return {
+//         ...state,
+//         categoriesMap: payload,
+//       };
+//     }
+//     default:
+//       throw new Error(`unhandled type ${type} in CategoryReducer`);
+//   }
+// };
+//
+// const INITIAL_STATE = {
+//   categoriesMap: {},
+// };
+
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
+  // const [{ categoriesMap }, dispatch] = useReducer(
+  //   categoryReducer,
+  //   INITIAL_STATE
+  // );
+
+  // const setCategoriesMap = (categoryMap) => {
+  //   dispatch({
+  //     type: CATEGORY_ACTIONS_TYPES.SET_CATEGORIES_MAP,
+  //     paylaod: categoryMap,
+  //   });
+  // };
 
   useEffect(() => {
     const getCategoriesMap = async () => {
